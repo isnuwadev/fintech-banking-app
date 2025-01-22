e7456697718517fd5f505db03be554c9ddc8c952
nuwagalb <isnuwa@gmail.com>
2025-01-22 16:26:00 +0300

    implement (root) layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+// import RecentTransactions from '@/components/RecentTransactions';
+import RightSidebar from '@/components/RightSidebar';
 
 const Home = () => {
+    const loggedIn = { firstName: 'Albert', lastName: 'Nuwagaba',  email: 'isnuwa@next.com'};
+
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250}
+          />
+        </header>
+
+        {/* <RecentTransactions 
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        /> */}
+      </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 123.50}, {currentBalance: 100.50}]}
+        // transactions={account?.transactions}
+        // banks={accountsData?.slice(0, 2)}
+      />
+    </section>
   )
 }
 
